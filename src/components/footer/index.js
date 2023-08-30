81eecd7f7e76949397ddcbf259a4acdc9cb34fb9
Mampi <1999mampidas@gmail.com>
2023-08-30 17:57:04 +0530

    Updates

--- src/components/footer/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./footer.css";
-const Footer = () => {
-	return <footer>Made with ❤️ by Mampi Das</footer>;
-};
-
-export default Footer;
--- src/redux/store/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers, createStore } from "redux";
-import { expenseReducer } from "../reducers/expenses";
-const reducer = combineReducers({
-	expenses: expenseReducer,
-});
-const initialState = {};
-const store = createStore(reducer, initialState);
-
-export default store;
